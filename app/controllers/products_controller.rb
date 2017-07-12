e029d4be3d39295fa7e24680e3fe4cefd171b69e
Theo Okelola <tokelola88@gmail.com>
2017-07-12 18:23:59 -0500

    mini-capstone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,29 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "index.html.erb"
   end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.new(title: params[:form_name], price: params[:form_price], description: params[:form_description], image: params[:form_image])
+    product.save
+    render "create.html.erb"
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "product#create"
+  get "/products/:id" => "products#show"
+  get "/products/:id/edit" => "products#edit"
 end
